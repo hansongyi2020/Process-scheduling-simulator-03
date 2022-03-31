d618cb652c909e45556c8e5da59316463199c18a
han songyi <hhan1218@naver.com>
2022-03-31 16:12:00 +0900

    hansongyi commit

--- gui.py
+++ gui.py
@@ -1,17 +1,2 @@
-from tkinter import *
-
-win = Tk()
-win.geometry("300x100")#크기
-win.title("what time?")#제목
-win.option_add("*Font", "궁서 20")#폰트
-
-def alert():
-    print("버튼이 눌림")
-
-btn = Button(win)#버튼 생성
-btn.config(text = "한송이")#내용
-btn.config(width = 10)#너비
-btn.config(command = alert)#
-
-btn.pack()
-win.mainloop() 
+dict = {"a" : [9, 8], "b" : [1 ,2]}
+print(dict[0])
---
+++ test99.py
@@ -0,0 +1,15 @@
+'''
+class process:
+    waiting_time = []
+    turnaround_time = []
+    normalized_time = []
+    average_time = 0
+    def __init__(self, AT, BT):#AT, BT :list
+        arrived_time = AT
+        burst_time = BT
+'''
+num = [1, 2, 3, 4, 5]
+num1 = ["hans", "Dd"]
+print(num + num1)
+print(3*num)
+print(3*num1)
